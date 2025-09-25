968c426ff5b2dd32fec40037611600b05de65f44
Highlandfury <cpapextom@gmail.com>
2025-09-25 14:23:54 +0100

    cvc

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,5 @@
 import { serve } from "inngest/next";
-import {
-  inngest,
-  syncUsercreation,
-  syncUserDeletion,
-  syncUserUpdation,
-} from "@/config/inngest";
+import {  inngest } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
